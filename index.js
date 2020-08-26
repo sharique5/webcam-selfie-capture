2d8525b84cf5606260c3de93320a97bca6368424
sharique5 <hiresharique@gmail.com>
2020-08-26 10:20:37 +0530

    scope update

--- index.js
+++ index.js
@@ -4,19 +4,23 @@ exports.webcamSelfieCapture = {
     video: null,
     startVideo: () => {
         navigator.mediaDevices.getUserMedia(
-            { video: {} },
-            stream => this.video.srcObject = stream,
-            err => console.error(err)
-        )
+            { video: {} })
+            .then(stream => {
+                this.webcamSelfieCapture.video.srcObject = stream
+            })
+            .catch(err => {
+                console.log(err.name + ": " + err.message);
+            })
+        
     },
     setVideoElementId: videoId => {
-        this.video = document.getElementById(videoId)
+        this.webcamSelfieCapture.video = document.getElementById(videoId)
     },
     setVideoElementEventListener: () => {
-        this.video.addEventListener('play', () => {
-            const canvas = faceapi.createCanvasFromMedia(this.video)
+        this.webcamSelfieCapture.video.addEventListener('play', () => {
+            const canvas = faceapi.createCanvasFromMedia(this.webcamSelfieCapture.video)
             document.body.append(canvas)
-            const displaySize = { width: this.video.width, height: this.video.height }
+            const displaySize = { width: this.webcamSelfieCapture.video.width, height: this.webcamSelfieCapture.video.height }
             faceapi.matchDimensions(canvas, displaySize)
             setInterval(async () => {
                 const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
